50ae3cea958d31ee285b4811705ff7942a841f9e
kiranrajvicdigit <kiranraj@vicdigit-technologies.com>
2023-11-09 15:44:55 -0600

    five commit

--- screens/Dashboard.js
+++ screens/Dashboard.js
@@ -2,168 +2,172 @@ import React, { useState } from 'react'
 import { View, Text, Button } from 'react-native'
 import CheckBox from '@react-native-community/checkbox';
 
+import firestore from '@react-native-firebase/firestore';
 
-const tasks=[{
-t_id:1,
+const usersCollection = firestore().collection('Users');
 
-  task_name:"task1",
-  task_description:"first task",
-  status:true,
+
+const tasks = [{
+  t_id: 1,
+
+  task_name: "task1",
+  task_description: "first task",
+  status: true,
 
 }
-,
-  {
-    t_id: 1,
-
-    task_name: "task1",
-    task_description: "first task",
-    status: true,
-
-  }
   ,
-  {
-    t_id: 1,
+{
+  t_id: 1,
 
-    task_name: "task1",
-    task_description: "first task",
-    status: true,
+  task_name: "task1",
+  task_description: "first task",
+  status: true,
 
-  }
+}
   ,
-  {
-    t_id: 1,
+{
+  t_id: 1,
 
-    task_name: "task1",
-    task_description: "first task",
-    status: true,
+  task_name: "task1",
+  task_description: "first task",
+  status: true,
 
-  }
+}
   ,
-  {
-    t_id: 1,
+{
+  t_id: 1,
 
-    task_name: "task1",
-    task_description: "first task",
-    status: true,
+  task_name: "task1",
+  task_description: "first task",
+  status: true,
 
-  }
+}
   ,
-  {
-    t_id: 1,
+{
+  t_id: 1,
 
-    task_name: "task1",
-    task_description: "first task",
-    status: true,
+  task_name: "task1",
+  task_description: "first task",
+  status: true,
 
-  }
+}
   ,
-  {
-    t_id: 1,
+{
+  t_id: 1,
 
-    task_name: "task1",
-    task_description: "first task",
-    status: true,
+  task_name: "task1",
+  task_description: "first task",
+  status: true,
 
-  }
+}
+  ,
+{
+  t_id: 1,
+
+  task_name: "task1",
+  task_description: "first task",
+  status: true,
+
+}
   , {
-    t_id: 1,
+  t_id: 1,
 
-    task_name: "task1",
-    task_description: "first task",
-    status: true,
+  task_name: "task1",
+  task_description: "first task",
+  status: true,
 
-  }
+}
   ,
 
-  {
-    t_id: 1,
+{
+  t_id: 1,
 
-    task_name: "task1",
-    task_description: "first task",
-    status: true,
+  task_name: "task1",
+  task_description: "first task",
+  status: true,
 
-  }
+}
   ,
-  {
-    t_id: 1,
+{
+  t_id: 1,
 
-    task_name: "task1",
-    task_description: "first task",
-    status: true,
+  task_name: "task1",
+  task_description: "first task",
+  status: true,
 
-  }
-  ,
-
-  {
-    t_id: 1,
-
-    task_name: "task1",
-    task_description: "first task",
-    status: true,
-
-  }
-  ,
-  {
-    t_id: 1,
-
-    task_name: "task1",
-    task_description: "first task",
-    status: true,
-
-  }
+}
   ,
 
-  {
-    t_id: 1,
+{
+  t_id: 1,
 
-    task_name: "task1",
-    task_description: "first task",
-    status: true,
+  task_name: "task1",
+  task_description: "first task",
+  status: true,
 
-  }
+}
   ,
-  {
-    t_id: 1,
+{
+  t_id: 1,
 
-    task_name: "task1",
-    task_description: "first task",
-    status: true,
+  task_name: "task1",
+  task_description: "first task",
+  status: true,
 
-  }
+}
+  ,
+
+{
+  t_id: 1,
+
+  task_name: "task1",
+  task_description: "first task",
+  status: true,
+
+}
+  ,
+{
+  t_id: 1,
+
+  task_name: "task1",
+  task_description: "first task",
+  status: true,
+
+}
   ,]
 
-  function Dashboard() {
-    const [value, setValue] = useState(tasks);
-    const updatetask=async=(index1,value)=>{
-setValue(prev=>{return prev.map((item,index)=>index===index1?{...item,status:value}:item)})
-    }
-    return (
-      <View>
-        <View  style={{ flexDirection: 'row', justifyContent: 'space-around' }}>
-          <Text>task name </Text>
-          <Text>task description</Text>
-          <Text>status</Text>
-        </View>
-        {
-         
-          value.map((item, index) => (
-            <View key={index} style={{flexDirection:'row',justifyContent:'space-around'}}>
-              <Text>{item.task_name}</Text>
-              <Text>{item.task_description}</Text>
-              <Text>{item.task_name}</Text>
-              <CheckBox
-                disabled={false}
-                value={item.status}
-                onValueChange={(value) => { updatetask(index,value) }
-                 
-                }
-              />
-            </View>
-          ))
-        }
-      </View>
-    );
+function Dashboard() {
+  const [value, setValue] = useState(tasks);
+  const updatetask = async = (index1, value) => {
+    setValue(prev => { return prev.map((item, index) => index === index1 ? { ...item, status: value } : item) })
   }
+  return (
+    <View>
+      <View style={{ flexDirection: 'row', justifyContent: 'space-around' }}>
+        <Text>task name </Text>
+        <Text>task description</Text>
+        <Text>status</Text>
+      </View>
+      {
+
+        value.map((item, index) => (
+          <View key={index} style={{ flexDirection: 'row', justifyContent: 'space-around' }}>
+            <Text>{item.task_name}</Text>
+            <Text>{item.task_description}</Text>
+            <Text>{item.task_name}</Text>
+            <CheckBox
+              disabled={false}
+              value={item.status}
+              onValueChange={(value) => { updatetask(index, value) }
+
+              }
+            />
+          </View>
+        ))
+      }
+    </View>
+  );
+}
 
 
 export default Dashboard
